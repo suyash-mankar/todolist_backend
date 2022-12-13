58a64a6ed9c758705c25b592853535105aefe64c
Suyash Mankar <suyashmankar@gmail.com>
2022-12-13 20:04:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 8000;
+
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/', require('./routes'));
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(err);
+    }
+    console.log('server running on port', port);
+})
